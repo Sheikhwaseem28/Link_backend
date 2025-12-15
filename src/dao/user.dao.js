268c643ed853b08e159d9e5b9d027fa7dc6f1aba
Sheikhwaseem28 <sheikhwaseem700800@gmail.com>
2025-12-15 15:14:34 +0530

    login fix

--- src/dao/user.dao.js
+++ src/dao/user.dao.js
@@ -1,28 +1,25 @@
-import { createUser, findUserByEmail, findUserByEmailByPassword } from "../dao/user.dao.js"
-import { ConflictError } from "../utils/errorHandler.js"
-import { signToken } from "../utils/helper.js"
+import User from "../models/user.model.js";
 
-export const registerUser = async (name, email, password) => {
-    const user = await findUserByEmail(email)
-    if (user) throw new ConflictError("User already exists")
-    const newUser = await createUser(name, email, password)
-    const token = await signToken({ id: newUser._id })
-    return { token, user: newUser }
-}
+/* ================= REGISTER ================= */
+export const createUser = async (name, email, password) => {
+  return await User.create({
+    name,
+    email,
+    password,
+  });
+};
 
-export const loginUser = async (email, password) => {
-    // Use findUserByEmailByPassword which includes the password field
-    const user = await findUserByEmailByPassword(email)
-    if (!user) throw new Error("Invalid email or password")
+/* ================= LOGIN ================= */
+export const findUserByEmail = async (email) => {
+  return await User.findOne({ email });
+};
 
-    const isPasswordValid = await user.comparePassword(password)
-    if (!isPasswordValid) throw new Error("Invalid email or password")
-    
-    const token = signToken({ id: user._id })
-    
-    // Remove password from user object before returning
-    const userWithoutPassword = user.toObject()
-    delete userWithoutPassword.password
-    
-    return { token, user: userWithoutPassword }
-}
+// Includes password field explicitly
+export const findUserByEmailByPassword = async (email) => {
+  return await User.findOne({ email }).select("+password");
+};
+
+/* ================= AUTH ================= */
+export const findUserById = async (id) => {
+  return await User.findById(id);
+};
